9c05f24f62e17348db1168562ce15819f64514b0
M. Simon Borg <msimonborg@gmail.com>
2017-08-04 12:25:48 -0400

    make benchmarking more equitable

--- benchmarks/allowable.rb
+++ benchmarks/allowable.rb
@@ -10,22 +10,26 @@ ActionController::Parameters.class_eval do
 end
 
 hash = {}
+allow_filters = {}
 
 1_000_000.times do |n|
   hash[n] = n
+  allow_filters[n] = n + 1
 end
+
+forbid_filters = hash
 
 params = ActionController::Parameters.new(hash)
 
 Benchmark.bmbm do |bm|
-  bm.report('Hash#allow') { hash.allow(hash) }
-  bm.report('Hash#forbid') { hash.forbid(hash) }
+  bm.report('Hash#allow') { hash.allow(allow_filters) }
+  bm.report('Hash#forbid') { hash.forbid(forbid_filters) }
 
   bm.report('ActionController::Parameters#allow') do
-    params.allow(hash)
+    params.allow(allow_filters)
   end
 
   bm.report('ActionController::Parameters#forbid') do
-    params.forbid(hash)
+    params.forbid(forbid_filters)
   end
 end
